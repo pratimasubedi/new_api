65b7a46fd84e4d85217450a686fcd0d0379099b6
Pratima Subedi <pratimasubedi77@gmail.com>
2023-05-23 16:55:48 +0545

    new_api

---
+++ lib/home_page.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:new_api/models/model.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<SamplePosts> samplePosts = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: samplePosts.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 150,
+                    color: Colors.greenAccent,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 10),
+                    margin: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'User id :${samplePosts[index].userId}',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Text(
+                          'Id:${samplePosts[index].id}',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Text(
+                          'Title :${samplePosts[index].title}',
+                          maxLines: 1,
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Text(
+                          'Body :${samplePosts[index].body}',
+                          maxLines: 1,
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  );
+                });
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
+  }
+
+  Future<List<SamplePosts>> getData() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> index in data) {
+        samplePosts.add(SamplePosts.fromJson(index));
+      }
+      return samplePosts;
+    } else {
+      return samplePosts;
+    }
+  }
+}
